61378566c3ed6547907063840f3a8807671f96c9
Seth Wheeler <wheelersc@cox.net>
2020-04-08 13:27:13 -0700

    edit tags and bugfixes

--- src/manager.js
+++ src/manager.js
@@ -10,16 +10,16 @@ const methods = require('./methods.js');
 
 const args = process.argv.splice(2);
 
-const alias = (args.includes('-a') || args.includes('-alias'))
-  ? ((args[args.indexOf('-alias')] && args[args.indexOf('-alias') + 1]) || (args[args.indexOf('-a')] && args[args.indexOf('-a') + 1])) : null;
-const email = (args.includes('-e') || args.includes('-email'))
-  ? ((args[args.indexOf('-email')] && args[args.indexOf('-email') + 1]) || (args[args.indexOf('-e')] && args[args.indexOf('-e') + 1])) : null;
-const passphrase = (args.includes('-p') || args.includes('-passphrase'))
-  ? ((args[args.indexOf('-passphrase')] && args[args.indexOf('-passphrase') + 1]) || (args[args.indexOf('-p')] && args[args.indexOf('-p') + 1])) : null;
-const bits = (args.includes('-b') || args.includes('-bits'))
-  ? ((args[args.indexOf('-bits')] && args[args.indexOf('-bits') + 1]) || (args[args.indexOf('-b')] && args[args.indexOf('-b') + 1])) : 4096;
-const dir = (args.includes('-d') || args.includes('-directory'))
-  ? ((args[args.indexOf('-directory')] && args[args.indexOf('-directory') + 1]) || (args[args.indexOf('-b')] && args[args.indexOf('-b') + 1])) : path.join(require('os').homedir(), '/.ssh');
+const alias = (args.includes('a') || args.includes('alias'))
+  ? ((args[args.indexOf('alias')] && args[args.indexOf('alias') + 1]) || (args[args.indexOf('a')] && args[args.indexOf('a') + 1])) : null;
+const email = (args.includes('e') || args.includes('email'))
+  ? ((args[args.indexOf('email')] && args[args.indexOf('email') + 1]) || (args[args.indexOf('e')] && args[args.indexOf('e') + 1])) : null;
+const passphrase = (args.includes('p') || args.includes('passphrase'))
+  ? ((args[args.indexOf('passphrase')] && args[args.indexOf('passphrase') + 1]) || (args[args.indexOf('p')] && args[args.indexOf('p') + 1])) : null;
+const bits = (args.includes('b') || args.includes('bits'))
+  ? ((args[args.indexOf('bits')] && args[args.indexOf('bits') + 1]) || (args[args.indexOf('b')] && args[args.indexOf('b') + 1])) : 4096;
+const dir = (args.includes('d') || args.includes('directory'))
+  ? ((args[args.indexOf('directory')] && args[args.indexOf('directory') + 1]) || (args[args.indexOf('b')] && args[args.indexOf('b') + 1])) : path.join(require('os').homedir(), '/.ssh');
 
 if (!fs.existsSync(path.join(require('os').homedir(), '.gitconfig'))) {
   console.log('Could not find a git configuration for the current user, please dowload git and try again');
@@ -42,9 +42,17 @@ if (args[0] === 'create-alias') {
 } else if (args[0] === 'change-alias') {
   if (!alias) {
     methods.chooseAlias(' to use', dir).then(async (_alias) => {
-      const newAlias = await methods.changeAlias(_alias, dir);
-      console.log(`${'Successfully changed alias' + '\n'
-                  + 'Current alias: '}${newAlias}`);
+      methods.changeAlias(_alias, dir).then((newAlias) => {
+        console.log(`${'Successfully changed alias' + '\n' + 'Current alias: '}${newAlias}`);
+      }).catch((err) => {
+        throw (err);
+      });
+    }).catch((err) => {
+      throw (err);
+    });
+  } else {
+    methods.changeAlias(alias, dir).then((newAlias) => {
+      console.log(`${'Successfully changed alias' + '\n' + 'Current alias: '}${newAlias}`);
     }).catch((err) => {
       throw (err);
     });
@@ -52,7 +60,17 @@ if (args[0] === 'create-alias') {
 } else if (args[0] === 'delete-alias') {
   if (!alias) {
     methods.chooseAlias(' to delete', dir).then(async (_alias) => {
-      const delAlias = await methods.deleteAlias(_alias, dir);
+      methods.deleteAlias(_alias, dir).then((delAlias) => {
+        console.log(`Successfully deleted alias: ${delAlias}` + '\n'
+                  + 'You may need to change you current alias, if you were using the alias you just deleted');
+      }).catch((err) => {
+        throw (err);
+      });
+    }).catch((err) => {
+      throw (err);
+    });
+  } else {
+    methods.deleteAlias(alias, dir).then((delAlias) => {
       console.log(`Successfully deleted alias: ${delAlias}` + '\n'
                 + 'You may need to change you current alias, if you were using the alias you just deleted');
     }).catch((err) => {
@@ -84,11 +102,11 @@ if (args[0] === 'create-alias') {
         + '  ' + 'delete-alias:       ' + ' ' + 'Deletes the public and private key for an alias' + '\n'
         + '  ' + 'h, help:            ' + ' ' + 'Print available command line commands and options (currently set)' + '\n\n'
         + 'Available Options:' + '\n'
-        + '  ' + '-a, -alias:     ' + ' ' + 'specify the alias' + '\n'
-        + '  ' + '-b, -bits:      ' + ' ' + 'specify the number of bits to create for the new key, defaults to 4096, the minimmum is 1024' + '\n'
-        + '  ' + '-d, -directory: ' + ' ' + 'specify the directory to create the new key(s) in, defaults to the .ssh folder in the home directory' + '\n'
-        + '  ' + '-e, -email:     ' + ' ' + 'specify the email' + '\n'
-        + '  ' + '-p, -passphrase:' + ' ' + 'specify the passphrase');
+        + '  ' + 'a, alias:     ' + ' ' + 'specify the alias' + '\n'
+        + '  ' + 'b, bits:      ' + ' ' + 'specify the number of bits to create for the new key, defaults to 4096, the minimmum is 1024' + '\n'
+        + '  ' + 'd, directory: ' + ' ' + 'specify the directory to create the new key(s) in, defaults to the .ssh folder in the home directory' + '\n'
+        + '  ' + 'e, email:     ' + ' ' + 'specify the email' + '\n'
+        + '  ' + 'p, passphrase:' + ' ' + 'specify the passphrase');
 } else {
   console.log(`Command: ${args[0]} not found, please try again with a different command or execute` + '\n'
         + '$ gam help' + '\n'
