a219ca34bbef1ce3ed305fb9c2a32b8aaffc2955
Seth Wheeler <wheelersc@cox.net>
2020-04-08 13:00:04 -0700

    remove logs from API

--- src/manager.js
+++ src/manager.js
@@ -17,7 +17,7 @@ const email = (args.includes('-e') || args.includes('-email'))
 const passphrase = (args.includes('-p') || args.includes('-passphrase'))
   ? ((args[args.indexOf('-passphrase')] && args[args.indexOf('-passphrase') + 1]) || (args[args.indexOf('-p')] && args[args.indexOf('-p') + 1])) : null;
 const bits = (args.includes('-b') || args.includes('-bits'))
-  ? ((args[args.indexOf('-bits')] && args[args.indexOf('-bits') + 1]) || (args[args.indexOf('-b')] && args[args.indexOf('-b') + 1])) : '4096';
+  ? ((args[args.indexOf('-bits')] && args[args.indexOf('-bits') + 1]) || (args[args.indexOf('-b')] && args[args.indexOf('-b') + 1])) : 4096;
 const dir = (args.includes('-d') || args.includes('-directory'))
   ? ((args[args.indexOf('-directory')] && args[args.indexOf('-directory') + 1]) || (args[args.indexOf('-b')] && args[args.indexOf('-b') + 1])) : path.join(require('os').homedir(), '/.ssh');
 
@@ -28,19 +28,33 @@ if (!fs.existsSync(path.join(require('os').homedir(), '.gitconfig'))) {
 }
 
 if (args[0] === 'create-alias') {
-  methods.createAlias(alias, email, passphrase, bits, dir);
+  methods.createAlias(alias, email, passphrase, bits, dir).then(async (newAlias) => {
+    console.log('\n' + 'Generating SSH Keys...');
+    await methods.generateKey(newAlias.alias, newAlias.email, newAlias.passphrase, newAlias.bits, newAlias.dir).then(async () => {
+      console.log('\n' + 'Adding SSH Keys to the SSH authentication agent...');
+      await methods.addSshKeyAgent(newAlias.alias, newAlias.dir);
+      console.log(`${'Successfully generated an SSH Key for the new git profile' + '\n'
+          + 'Your new public and private keys can be found here: '}${newAlias.dir}, and are called id_rsa_${newAlias.alias} and id_rsa_${newAlias.alias}.pub`
+          + '\n\n'
+          + 'Please copy your key to your prefered git repository to begin using it.');
+    });
+  });
 } else if (args[0] === 'change-alias') {
   if (!alias) {
-    methods.chooseAlias(' to use', dir).then((_alias) => {
-      methods.changeAlias(_alias, dir);
+    methods.chooseAlias(' to use', dir).then(async (_alias) => {
+      const newAlias = await methods.changeAlias(_alias, dir);
+      console.log(`${'Successfully changed alias' + '\n'
+                  + 'Current alias: '}${newAlias}`);
     }).catch((err) => {
       throw (err);
     });
   }
 } else if (args[0] === 'delete-alias') {
   if (!alias) {
-    methods.chooseAlias(' to delete', dir).then((_alias) => {
-      methods.deleteAlias(_alias, dir);
+    methods.chooseAlias(' to delete', dir).then(async (_alias) => {
+      const delAlias = await methods.deleteAlias(_alias, dir);
+      console.log(`Successfully deleted alias: ${delAlias}` + '\n'
+                + 'You may need to change you current alias, if you were using the alias you just deleted');
     }).catch((err) => {
       throw (err);
     });
@@ -53,7 +67,13 @@ if (args[0] === 'create-alias') {
         + '  ' + 'Local:' + '\n'
         + '    ' + 'Email:' + ` ${emails.localEmail}`);
 } else if (args[0] === 'backup') {
-  methods.backup(dir);
+  try {
+    const folderName = methods.backup(dir);
+    console.log(`Created Backup folder named: ${folderName}` + ' successfully.');
+  } catch (e) {
+    console.log('An error occured when creating the backup folder');
+    throw e;
+  }
 } else if (args[0] === 'h' || args[0] === 'help' || args.length === 0) {
   console.log('Usage: gam [command] [options]' + '\n\n'
         + 'Available Commands:' + '\n'
--- src/methods.js
+++ src/methods.js
@@ -58,9 +58,8 @@ function backup(dir = path.join(require('os').homedir(), '/.ssh')) {
   const folderName = `backup-${getFormattedDate(new Date())}`;
   try {
     copyFolderRecursiveSync(dir, path.join(dir, `/${folderName}`));
-    console.log(`Created Backup folder named: ${folderName}` + ' successfully.');
+    return folderName;
   } catch (e) {
-    console.log('An error occured when creating the backup folder');
     throw e;
   }
 }
@@ -88,7 +87,7 @@ function chooseAlias(customStr, dir = path.join(require('os').homedir(), '/.ssh'
   });
 }
 
-function generateKey(alias, email, passphrase, bits, dir = path.join(require('os').homedir(), '/.ssh')) {
+function generateKey(alias, email, passphrase, bits = 4096, dir = path.join(require('os').homedir(), '/.ssh')) {
   return new Promise(((resolve, reject) => {
     exec(`ssh-keygen -f ${path.join(dir, `id_rsa_${alias}`)} -t rsa -b ${bits} -C ${email} -N ${passphrase || "''"} -m PEM`, (error, stdout, stderr) => {
       if (error) {
@@ -170,168 +169,190 @@ function addSshKeyAgent(alias, dir = path.join(require('os').homedir(), '/.ssh')
   }));
 }
 
-async function createAlias(alias, email, passphrase, bits, dir = path.join(require('os').homedir(), '/.ssh')) {
+async function createAlias(alias, email, passphrase, bits = 4096, dir = path.join(require('os').homedir(), '/.ssh')) {
   if (fs.existsSync(path.join(dir, `id_rsa_${alias}.pub`)) || fs.existsSync(path.join(dir, `id_rsa_${alias}`))) {
     console.log(`An SSH key already exsists for the alias: ${alias}, please choose a different alias and try again`);
     alias = null;
-  } else {
-    if (!alias) {
-      await new Promise((resolve, reject) => {
-        inquirer.prompt({
-          type: 'input',
-          name: 'alias',
-          message: 'Please enter an alias to be associated with this new git profile:',
-        })
-          .then(async (answer) => {
-            alias = answer.alias;
-            while (fs.existsSync(path.join(dir, `id_rsa_${alias}.pub`)) || fs.existsSync(path.join(dir, `id_rsa_${alias}`))) {
-              await inquirer.prompt({
-                type: 'input',
-                name: 'alias',
-                message: 'An SSH key already exsists for the alias, please enter different alias:',
-              })
-                .then((answer2) => {
-                  alias = answer2.alias;
-                })
-                .catch((error) => {
-                  reject(error);
-                });
-            }
-            resolve();
-          })
-          .catch((error) => {
-            reject(error);
-          });
-      });
-    }
-    if (!email) {
-      await new Promise((resolve, reject) => {
-        inquirer.prompt({
-          type: 'input',
-          name: 'email',
-          message: 'Please enter an email, to be associated with this new git profile:',
-        })
-          .then(async (answer) => {
-            email = answer.email;
-            while (!email.match(/\S+@\S+\.\S+/g)) {
-              await inquirer.prompt({
-                type: 'input',
-                name: 'email',
-                message: `${email} is not a valid email, please enter a valid email to be associated with this new git profile:`,
-              })
-                .then((answer2) => {
-                  email = answer2.email;
-                })
-                .catch((error) => {
-                  reject(error);
-                });
-            }
-            resolve();
-          })
-          .catch((error) => {
-            reject(error);
-          });
-      });
-    }
-    if (!passphrase) {
-      await new Promise((resolve, reject) => {
-        inquirer.prompt({
-          type: 'password',
-          name: 'passphrase',
-          message: 'Please enter a passphrase (leave empty for no passphrase):',
-        })
-          .then(async (answer1) => {
-            await inquirer.prompt({
-              type: 'password',
-              name: 'passphrase',
-              message: 'Please re-enter the passphrase (leave empty for no passphrase):',
-            })
-              .then(async (answer2) => {
-                if (answer1.passphrase === answer2.passphrase) {
-                  passphrase = answer1.passphrase;
-                  resolve();
-                } else {
-                  await inquirer.prompt({
-                    type: 'password',
-                    name: 'passphrase',
-                    message: 'Your passphrases did not match' + '\n'
-                                                + 'Please re-enter the passphrase (leave empty for no passphrase):',
-                  })
-                    .then((answer3) => {
-                      if (answer1.passphrase === answer3.passphrase) {
-                        passphrase = answer1.passphrase;
-                        resolve();
-                      } else {
-                        console.log('Your passphrases did not match' + '\n'
-                                                    + 'Exiting the program due to too many failed attempts');
-                      }
-                    })
-                    .catch((error) => {
-                      reject(error);
-                    });
-                }
-              })
-              .catch((error) => {
-                reject(error);
-              });
-          })
-          .catch((error) => {
-            reject(error);
-          });
-      });
-    }
-    console.log('\n' + 'Generating SSH Keys...');
-    await generateKey(alias, email, passphrase, bits, dir);
-    console.log('\n' + 'Adding SSH Keys to the SSH authentication agent...');
-    await addSshKeyAgent(alias, dir);
-    console.log(`${'Successfully generated an SSH Key for the new git profile' + '\n'
-            + 'Your new public and private keys can be found here: '}${dir}, and are called id_rsa_${alias} and id_rsa_${alias}.pub`
-            + '\n\n'
-            + 'Please copy your key to your prefered git repository to begin using it.');
   }
-}
-
-async function changeAlias(alias, dir = path.join(require('os').homedir(), '/.ssh')) {
-  const pubKey = (await fs.readFileSync(path.join(dir, `id_rsa_${alias}.pub`)));
-  const email = pubKey.toString().match(/\S+@\S+\.\S+/g);
-  const promises = [
-    fs.writeFileSync(path.join(dir, 'id_rsa.pub'), pubKey),
-    fs.writeFileSync(path.join(dir, 'id_rsa'), fs.readFileSync(path.join(dir, `id_rsa_${alias}`))),
-  ];
-  Promise.all(promises).then(async () => {
-    const promises2 = [
-      changeGlobalEmail(email),
-      changeLocalEmail(email),
+  return new Promise(async (resolve, reject) => {
+    const promises = [
+      await new Promise((resolve, reject) => {
+        if (!alias) {
+          inquirer.prompt({
+            type: 'input',
+            name: 'alias',
+            message: 'Please enter an alias to be associated with this new git profile:',
+          })
+            .then(async (answer) => {
+              alias = answer.alias;
+              while (fs.existsSync(path.join(dir, `id_rsa_${alias}.pub`)) || fs.existsSync(path.join(dir, `id_rsa_${alias}`)) || alias === '') {
+                let message;
+                if (alias === '') {
+                  message = `${alias} is not a valid alias name, please enter a different name:`;
+                } else {
+                  message = 'An SSH key already exsists for the alias, please enter different alias:';
+                }
+                await inquirer.prompt({
+                  type: 'input',
+                  name: 'alias',
+                  message,
+                })
+                  .then((answer2) => {
+                    alias = answer2.alias;
+                  })
+                  .catch((error) => {
+                    reject(error);
+                  });
+              }
+              resolve();
+            })
+            .catch((error) => {
+              reject(error);
+            });
+        } else {
+          resolve();
+        }
+      }),
+      await new Promise((resolve, reject) => {
+        if (!email) {
+          inquirer.prompt({
+            type: 'input',
+            name: 'email',
+            message: 'Please enter an email, to be associated with this new git profile:',
+          })
+            .then(async (answer) => {
+              email = answer.email;
+              while (!email.match(/\S+@\S+\.\S+/g)) {
+                await inquirer.prompt({
+                  type: 'input',
+                  name: 'email',
+                  message: `${email} is not a valid email, please enter a valid email to be associated with this new git profile:`,
+                })
+                  .then((answer2) => {
+                    email = answer2.email;
+                  })
+                  .catch((error) => {
+                    reject(error);
+                  });
+              }
+              resolve();
+            })
+            .catch((error) => {
+              reject(error);
+            });
+        } else {
+          resolve();
+        }
+      }),
+      await new Promise((resolve, reject) => {
+        if (!passphrase) {
+          inquirer.prompt({
+            type: 'password',
+            name: 'passphrase',
+            message: 'Please enter a passphrase (leave empty for no passphrase):',
+          })
+            .then(async (answer1) => {
+              await inquirer.prompt({
+                type: 'password',
+                name: 'passphrase',
+                message: 'Please re-enter the passphrase (leave empty for no passphrase):',
+              })
+                .then(async (answer2) => {
+                  if (answer1.passphrase === answer2.passphrase) {
+                    passphrase = answer1.passphrase;
+                    resolve();
+                  } else {
+                    await inquirer.prompt({
+                      type: 'password',
+                      name: 'passphrase',
+                      message: 'Your passphrases did not match' + '\n'
+                                                + 'Please re-enter the passphrase (leave empty for no passphrase):',
+                    })
+                      .then((answer3) => {
+                        if (answer1.passphrase === answer3.passphrase) {
+                          passphrase = answer1.passphrase;
+                          resolve();
+                        } else {
+                          throw (new Error('Your passphrases did not match' + '\n'
+                                                    + 'Exiting the program due to too many failed attempts'));
+                          process.exit(1);
+                        }
+                      })
+                      .catch((error) => {
+                        reject(error);
+                      });
+                  }
+                })
+                .catch((error) => {
+                  reject(error);
+                });
+            })
+            .catch((error) => {
+              reject(error);
+            });
+        } else {
+          resolve();
+        }
+      }),
     ];
-    Promise.all(promises2).then(() => {
-      console.log(`${'Successfully changed alias' + '\n'
-                + 'Current alias: '}${alias}`);
-    }).catch((err) => {
-      throw (err);
+    Promise.all(promises).then(() => {
+      console.log(alias);
+      resolve({
+        alias,
+        email,
+        passphrase,
+        bits,
+        dir,
+      });
     });
-  }).catch((err) => {
-    throw (err);
   });
 }
 
+function changeAlias(alias, dir = path.join(require('os').homedir(), '/.ssh')) {
+  return new Promise((async (resolve, reject) => {
+    const pubKey = (await fs.readFileSync(path.join(dir, `id_rsa_${alias}.pub`)));
+    const email = pubKey.toString().match(/\S+@\S+\.\S+/g);
+    const promises = [
+      fs.writeFileSync(path.join(dir, 'id_rsa.pub'), pubKey),
+      fs.writeFileSync(path.join(dir, 'id_rsa'), fs.readFileSync(path.join(dir, `id_rsa_${alias}`))),
+    ];
+    Promise.all(promises).then(() => {
+      const promises2 = [
+        changeGlobalEmail(email),
+        changeLocalEmail(email),
+      ];
+      Promise.all(promises2).then(() => {
+        resolve(alias);
+      }).catch((err) => {
+        throw (err);
+      });
+    }).catch((err) => {
+      throw (err);
+    });
+  }));
+}
+
 function deleteAlias(alias, dir = path.join(require('os').homedir(), '/.ssh')) {
-  const promises = [
-    fs.unlinkSync(path.join(dir, `id_rsa_${alias}.pub`)),
-    fs.unlinkSync(path.join(dir, `id_rsa_${alias}`)),
-  ];
-  Promise.all(promises).then(async () => {
-    console.log('Successfully deleted alias' + '\n'
-            + 'You may need to change you current alias, if you were using the alias you just deleted');
-  }).catch((err) => {
-    throw (err);
+  return new Promise(async (resolve, reject) => {
+    const promises = [
+      fs.unlinkSync(path.join(dir, `id_rsa_${alias}.pub`)),
+      fs.unlinkSync(path.join(dir, `id_rsa_${alias}`)),
+    ];
+    Promise.all(promises).then(() => {
+      resolve(alias);
+    }).catch((err) => {
+      throw (err);
+    });
   });
 }
 
 module.exports = {
+  addSshKeyAgent,
   backup,
   chooseAlias,
   createAlias,
   changeAlias,
-  deleteAlias,
   currentAliasEmail,
+  deleteAlias,
+  generateKey,
 };
