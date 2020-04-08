4c92f7ea84104ebe6095d11513fe4ae6290192b5
Seth Wheeler <wheelersc@cox.net>
2020-04-08 00:48:14 -0700

    Update linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,27 @@
 module.exports = {
-    env: {
-        commonjs: true,
-        es6: true,
-        node: true,
-    },
-    extends: [
-        'airbnb-base',
-    ],
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
-    },
-    parserOptions: {
-        ecmaVersion: 2018,
-    },
-    rules: {
-        "import/order": "off",
-        "global-require": "off",
-        "no-useless-concat": "off",
-        "no-param-reassign": "off",
-        "no-loop-func": "off",
-        "no-await-in-loop": "off",
-        "no-console": "off"
-    },
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    'import/order': 'off',
+    'global-require': 'off',
+    'no-useless-concat': 'off',
+    'no-param-reassign': 'off',
+    'no-loop-func': 'off',
+    'no-await-in-loop': 'off',
+    'no-console': 'off',
+    'prefer-destructuring': 'off',
+  },
 };
